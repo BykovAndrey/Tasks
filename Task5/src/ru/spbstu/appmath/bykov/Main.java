3ef53fa420d2a6cda4528e40cadadc49e04ec3ec
BykovAndrey <andreas_bykov@mail.ru>
2016-01-19 22:38:50 +0300

    create ReadFile

--- Task5/src/ru/spbstu/appmath/bykov/Main.java
+++ Task5/src/ru/spbstu/appmath/bykov/Main.java
@@ -1,9 +1,27 @@
 package ru.spbstu.appmath.bykov;
 
-import java.io.File;
+import com.sun.corba.se.impl.presentation.rmi.ExceptionHandlerImpl;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        final File file1 = new File("matrix-dir","matrix1.txt");
+        final File file2 = new File("matrix-dir","matrix2.txt");
+            if (file1.exists() && file2.exists()) {
+                System.out.print("OK!");
+            }
+    }
+    private static String ReadFile(final File file) {
+        final StringBuilder result = new StringBuilder();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                result.append(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return result.toString();
     }
 }
