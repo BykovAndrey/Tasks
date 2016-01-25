05c5afddbc0d46df534c86a41b07361af0c2e72e
BykovAndrey <andreas_bykov@mail.ru>
2016-01-25 12:37:06 +0300

    Create project & create main class

---
+++ Task3/src/main/java/ru/spbstu/appmath/bykov/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 25.01.2016.
+ */
+package ru.spbstu.appmath.bykov;
+
+public class Main {
+
+}
--- Task5/src/ru/spbstu/appmath/bykov/Main.java
+++ Task5/src/ru/spbstu/appmath/bykov/Main.java
@@ -9,15 +9,15 @@ public class Main {
     public static void main(String[] args) {
         final File file1 = new File("matrix-dir","matrix1.txt");
         final File file2 = new File("matrix-dir","matrix2.txt");
-            if (file1.exists() && file2.exists()) {
-                System.out.print("OK!");
-            }
+        String matrix1 = ReadFile(file1);
+        String matrix2 = ReadFile(file2);
+        System.out.print(matrix1);
     }
     private static String ReadFile(final File file) {
         final StringBuilder result = new StringBuilder();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                result.append(scanner.nextLine());
+                result.append(scanner.nextLine() + " ");
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
