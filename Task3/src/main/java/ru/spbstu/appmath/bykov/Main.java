bb334807df522cbb8bccb8372465dd439f8e59b1
BykovAndrey <andreas_bykov@mail.ru>
2016-01-25 13:04:54 +0300

    add parse var value

--- Task3/src/main/java/ru/spbstu/appmath/bykov/Main.java
+++ Task3/src/main/java/ru/spbstu/appmath/bykov/Main.java
@@ -4,5 +4,15 @@
 package ru.spbstu.appmath.bykov;
 
 public class Main {
-
+public void main(String[] args) {
+    double var;
+    if(args.length == 2) {
+        var = Double.parseDouble(args[1]);
+    }else if (args.length == 1) {
+        var = 0;
+    }else {
+        System.out.println("Argiments Error");
+        return;
+    }
+}
 }
