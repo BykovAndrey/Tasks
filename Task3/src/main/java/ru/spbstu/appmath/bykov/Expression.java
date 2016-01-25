92400adcf4ee023d393f2f74c131523d5d3fc616
BykovAndrey <andreas_bykov@mail.ru>
2016-01-25 12:41:30 +0300

    Create interface Expression, class Var

---
+++ Task3/src/main/java/ru/spbstu/appmath/bykov/Expression.java
@@ -0,0 +1,8 @@
+package ru.spbstu.appmath.bykov;
+
+/**
+ * Created by Андрей on 25.01.2016.
+ */
+    public interface Expression {
+        double calc(double x) throws Exception;
+    }
---
+++ Task3/src/main/java/ru/spbstu/appmath/bykov/Var.java
@@ -0,0 +1,10 @@
+package ru.spbstu.appmath.bykov;
+
+/**
+ * Created by Андрей on 25.01.2016.
+ */
+public class Var implements Expression {
+    public double calc(double var) {
+        return var;
+    }
+}
